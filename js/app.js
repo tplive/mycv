4c73b62a79121fbd5f8921c1fb42b3aefa16276f
Thomas Qvidahl <tp@live.no>
2017-10-27 20:30:36 +0200

    Update experience text.

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ $(function() {
             employer : "EVRY Card Services AS",
             period : "Nov 2012 - Present",
             location : "Mo i Rana, Norway",
-            summary : "<ul><li>I was hired as a DBA, and worked extensively with SQL and other Microsoft technologies.</li><li>Consulted on ITIL processes and DevOps approach as tools for effective software development and IT operations.</li><li>Studying at Nord university, I took a class on Android mobile development. I was already falling in love with Java, and sparks flew! When the opportunity arose, I took the chance - moving to software development full-time! I'm currently working in the Technology and Innovation department on a native Android app prototype for our clients.</li><li>Experience with Java; Android SDK and Spring. Javascript; Node.js / Express and client-side with jQuery.</li><li>I practice what I preach; agile development and modern development.</li></ul>"
+            summary : "<ul><li>I was hired as a DBA, and worked extensively with SQL and other Microsoft technologies.</li><li>Consulted on ITIL processes and DevOps approach as tools for effective software development and IT operations.</li><li>Studying at Nord university, I took a class on Android mobile development. I was already falling in love with Java, and sparks flew! When the opportunity arose, I took the chance - moving to software development full-time! I'm currently working in the Technology and Innovation department on a native Android app prototype for our clients.</li><li>Experience with Java; Android SDK and Spring. Javascript; Node.js / Express and client-side with jQuery.</li></ul>"
         },
         {
             title : "Senior Consultant",
