bbb06ed972700d247509686d3badc28d4c1837a2
Thomas Qvidahl <tp@live.no>
2019-01-13 21:37:07 +0100

    Some minor changes. Updated with latest experience.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,47 @@
 
 $(function() {
     // The data in JSON format
-    var data = { experiences : [
+    // TODO Get from a file
+    var data = { "experiences" : [
         {
-            title : "Developer, Senior Consultant",
-            employer : "EVRY Card Services AS",
-            period : "Nov 2012 - Present",
-            location : "Mo i Rana, Norway",
-            summary : "<ul><li>I was hired as a DBA, and worked extensively with SQL and other Microsoft technologies.</li><li>Consulted on ITIL processes and DevOps approach as tools for effective software development and IT operations.</li><li>Studying at Nord University, I took a class on Android mobile development. I was already falling in love with Java, and sparks flew! When the opportunity came along, I took a chance, moving to software development full-time! Currently working in the Technology and Innovation department on a native Android app for our clients, utilizing contactless technology and cloud services.</li><li>Gained experience with Java; Android SDK and Spring. Also Javascript; Node.js / Express and client-side JS with jQuery and friends.</li><li>Taking a full-time study at Nord University, planned graduation in Spring 2018.</li></ul>"
-        },        {
-            title : "Teacher",
-            employer : "Høgskolen i Nesna",
-            period : "Spring semester of 2009, 2011, 2013, 2014, 2015",
-            location : "Mo i Rana, Norway",
-            summary : "<ul><li>Hired to teach \"Operating Systems - Configuration and Use\" at Community College.</li><li>Mainly focusing on virtualization, networking and interoperating Windows and Linux operating systems in heterogeneous environments.</li><li>Received great feedback from students, as a committed, competent and ambitious teacher.</li><li>Adviced several students on career choice, and many have been employed as per my advice.</li></ul>"
+            "title" : "DevOps Engineer",
+            "employer" : "Telenor Norge AS",
+            "period" : "Nov 2018 - Present",
+            "location" : "Trondheim, Norway",
+            "summary" : "I've been tasked with automating the installation of API platform Apigee (a Google product) using Ansible. Task includes installing, upgrading and maintenance of the platform. Enviroment separated in three; local machine Dev installation, AWS test installation and VMWare Prod installation."
         },
         {
-            title : "Senior Consultant",
-            employer : "iTet AS",
-            period : "Nov 2002 - Nov 2012",
-            location : "Mo i Rana, Norway",
-            summary : "<ul><li>Trusted advisor for several SMB and Enterprise clients all over Norway, working extensively on diverse deployments of Microsoft software and services such as SharePoint, Exchange SQL Server and Active Directory environments.</li><li>Trained as Microsoft Certified Trainer (MCT), Microsoft Certified IT Professional (MCITP) and others.</li><li>In-house consultant for all matters related to PKI and digital certificates.</li><li>Consistently Top 10 Consultant (by revenue) for many years. Won \"Employee of the Year\" in 2008.</li><li>Mentored and coached colleagues and clients in technology and career choices.</li><li>ITIL Foundation certified since 2011.</li></ul>"
+            "title" : "Developer",
+            "employer" : "Acando AS",
+            "period" : "Apr 2018 - Oct 2018",
+            "location" : "Trondheim, Norway",
+            "summary" : "I worked on a project for the Norwegian Geological Survey, building an API for their registation product for Geophysical surveys. We built the backend on microservices architecture, running on Spring, using an Oracle / ESRI database backend. Also worked on maintenance of their Android app for Well-registration."
         },
         {
-            title : "Consultant",
-            employer : "Telenor",
-            period : "September 1995 - October 2002",
-            location : "Trondheim, Norway",
-            summary : "<ul><li>Got started in IT after finishing vocational school (Computer technology, Stjørdal VGS).</li><li>I worked as a service technician with PC / Server / Storage / Network support for Telenor's clients.</li><li>In 1999 I joined the internal IT Operations department, responsible for keeping all of Telenor's server infrastructure running.</li><li>I was given Microsoft MCSE and other technical certifications by my employer.</li></ul>"
-        }
-    ]};
+            "title" : "Developer, Senior Consultant",
+            "employer" : "EVRY Card Services AS",
+            "period" : "Nov 2012 - Present",
+            "location" : "Mo i Rana, Norway",
+            "summary" : "<ul><li>I was hired as a DBA, and worked extensively with SQL and other Microsoft technologies.</li><li>Consulted on ITIL processes and DevOps approach as tools for effective software development and IT operations.</li><li>Studying at Nord university, I took a class in Android mobile development and that ignited a spark in me! When the opportunity arose, I took a chance on moving to software development, working in the Technology and Innovation department with an app prototype for our clients.</li></ul>"
+        },
+            {
+                "title" : "Senior Consultant",
+                "employer" : "iTet AS",
+                "period" : "Nov 2002 - Nov 2012",
+                "location" : "Mo i Rana, Norway",
+                "summary" : "<ul><li>I was a trusted advisor for many SMB and Enterprise clients in the Helgeland area, working extensively on large deployments of Microsoft techologies such as SharePoint, Exchange SQL Server and Active Directory environments</li><li>Trained as Microsoft Certified Trainer (MCT), Microsoft Certified IT Professional (MCITP) and others.</li><li>I was the company goto-guy for matters concerning PKI and digital certificates.</li><li>Consistently Top 10 Consultant by revenue for many years. \"Employee of the Year\" in 2008</li><li>Mentored and coached colleagues and clients in technology and career choices.</li><li>ITIL Foundation certified since 2011</li></ul>"
+            },
+            {
+                "title" : "Teacher",
+                "employer" : "Høgskolen i Nesna",
+                "period" : "Spring semester of 2009, 2011, 2013, 2014, 2015",
+                "location" : "Mo i Rana, Norway",
+                "summary" : "<ul><li>Hired to teach \"Operating Systems - Configuration and Use\" at Community College.</li><li>Mainly focusing on virtualization, networking Windows Server operating systems and interoperability with Linux in heterogeneous environments.</li><li>Received great feedback from students, as a committed, competent and ambitious teacher.</li><li>Adviced several students on career choice, and many have been employed as per my advice.</li></ul>"
+            }
+        ]
+    };
 
+    // TODO Get from a file
     var lang = { languages : [
         {
             language : "Norwegian",
@@ -42,7 +53,11 @@ $(function() {
         },
         {
             language: "Java",
-            proficiency : 70
+            proficiency : 90
+        },
+        {
+            language: "Kotlin",
+            proficiency : 50
         },
         {
             language : "HTML",
